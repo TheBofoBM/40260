ac897c82aa8a52e87bc65824d7a5510f3ec740f9
TheBofoBM <zs23014140@estudiantes.uv.mx>
2025-05-05 12:51:37 -0600

    08-fieldset

--- html00/formulario.js
+++ html00/formulario.js
@@ -4,7 +4,7 @@ const boton = document.getElementById("enviar")
 boton.addEventListener("click",
     () => {
         console.log("los parameetros")
-        let user = document.getElementById("suer")
+        let username = document.getElementById("username")
         let password = document.getElementById("password")
         console.Log (password.value)
         console.log(user.value)
